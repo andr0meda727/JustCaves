2ddbe91428967f49635588a8b59699ab8bc5b84c
andr0meda727 <kwwk747@gmail.com>
2026-01-04 00:17:31 +0100

    Add redirect from empty path to /caves

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ require 'Routing.php';
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
+if (empty($path)) {
+    header("Location: /caves");
+    exit();
+}
+
 Routing::run($path);
 
 ?>
